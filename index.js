34762e6ce52fe290565bbc6f806d9e73c616a67e
alhammanazil <alhammanazil18@gmail.com>
2024-02-06 11:12:03 +0700

    make model & seed for sample data

---
+++ index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// Models
+const Product = require('./models/product');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1/shop_db')
+.then((result) => {
+    console.log('Connected to MongoDB');
+})
+.catch(err => {
+    console.error('Error connecting to MongoDB', err);
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => {
+    console.log('Shop App listening on http://127.0.0.1:3000')
+});
