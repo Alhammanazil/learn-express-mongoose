cb272995befa8b3ed1ed528f9e1b28cd1f6afa2c
alhammanazil <107827210+Alhammanazil@users.noreply.github.com>
2024-02-06 22:47:13 +0700

    create restful product update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const app = express();
 
@@ -18,6 +19,7 @@ mongoose.connect('mongodb://127.0.0.1/shop_db')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -48,6 +50,12 @@ app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     res.render('products/edit', { product });
+});
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+    res.redirect(`/products/${product._id}`);
 });
 
 
