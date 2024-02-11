25c296ce3fee44bc3c5948e00ad9e20b54a64694
alhammanazil <107827210+Alhammanazil@users.noreply.github.com>
2024-02-11 20:53:26 +0700

    save a data product to garment model

--- index.js
+++ index.js
@@ -50,9 +50,29 @@ app.post('/garments', wrapAsync(async (req, res) => {
 
 app.get('/garments/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
-    const garment = await Garment.findById(id).populate('products');
+    const garment = await Garment.findById(id)
     res.render('garment/show', { garment });
 }));
+
+// /garments/:garment_id/products/create
+app.get('/garments/:garment_id/products/create', (req, res) => {
+    const { garment_id } = req.params;
+    res.render('products/create', { garment_id });
+});
+
+// /garments/:garment_id/products
+app.post('/garments/:garment_id/products', wrapAsync(async (req, res) => {
+    const { garment_id } = req.params;
+    const garment = await Garment.findById(garment_id);
+    const product = new Product(req.body);
+    garment.products.push(product);
+    product.garment = garment;
+    await product.save();
+    await garment.save();
+    console.log(garment);
+    res.redirect(`/garments/${garment._id}`);
+}));
+
 
 app.get('/products', async (req, res) => {
     try {
