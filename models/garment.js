2e188b9585469e8b593c68e6a0328b20719b5c68
alhammanazil <107827210+Alhammanazil@users.noreply.github.com>
2024-02-11 19:07:13 +0700

    models for garment and products

---
+++ models/garment.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const garmentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Garment name is required']
+    },
+    location: {
+        type: String,
+    },
+    contact: {
+        type: Number,
+        required: [true, 'Contact is required']
+    },
+    products: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }]
+})
